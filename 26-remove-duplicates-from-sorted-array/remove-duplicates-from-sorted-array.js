f0f6b1ae5df4453123d2d1274ff8fa28de3bc00d
Tushar Tyagi <152903592+tushartyagi0507@users.noreply.github.com>
2025-06-23 22:09:08 +0530

    Time: 0 ms (100.00%) | Memory: 57.5 MB (64.27%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let x = 0;
+    for (let i=0; i < nums.length; i++){
+        if(nums[i] > nums[x]){
+           x = x + 1
+            nums[x] = nums[i]
+        }
+    }
+    return x+1
+};
+
+
+const nums = [0,0,1,1,1,2,2,3,3,4]
+
+removeDuplicates(nums)
