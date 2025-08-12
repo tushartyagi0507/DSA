9a91a6bcf9d0f3ec1fd331c45c4a0de9f9bcc8f9
Tushar Tyagi <152903592+tushartyagi0507@users.noreply.github.com>
2025-08-13 00:23:59 +0530

    Time: 1 ms (87.98%) | Memory: 55.5 MB (31.98%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -3,35 +3,15 @@
  * @param {number} target
  * @return {number[]}
  */
-// var twoSum = function(nums, target) {
-//     let i = 0;
-//     let n = nums.length;
-//     let map = {}
-//     for(i; i < n ; i++){
-//         map[nums[i]] = i
-//     }
-
-//     for(i; i < n; i++){
-//         let pairToFind = target - nums[i];
-//         if(map[pairTofind] && map[pairToFind] != i){
-//             return [i, map[pairToFind]]
-//         }
-//     }
-// };
-
-
 var twoSum = function(nums, target) {
-    let n = nums.length;
-    let map = {};
-    
-    for(let i = 0; i < n; i++) {
-        map[nums[i]] = i;
-    }
+    let n= nums.length
+    let hash = {}
 
-    for(let i = 0; i < n; i++) {
-        let pairToFind = target - nums[i];
-        if (pairToFind in map && map[pairToFind] !== i) {
-            return [i, map[pairToFind]];
+    for(let i=0; i< n; i++){
+        let pair = target - nums[i]
+        if(hash[pair] !== undefined){
+            return [hash[pair], i]
         }
+            hash[nums[i]] = i;
     }
-}; 
+};
