2b062db3b2bab0b53f3e3b30ecf1a7f563ddd234
Tushar Tyagi <152903592+tushartyagi0507@users.noreply.github.com>
2026-04-03 16:29:42 +0530

    Add 144_binary_tree_preorder_traversal.js - LeetCode Problem 144

---
+++ 144_binary_tree_preorder_traversal.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+    const ans = []
+    function preorder(node){
+        if(node == null) return;
+        ans.push(node?.val)
+        preorder(node?.left)
+        preorder(node?.right)
+    }
+    preorder(root)
+    return ans;
+};
