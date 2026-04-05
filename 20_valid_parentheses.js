df79ae3dfbf4c845469024098096f44812bd38c9
Tushar Tyagi <152903592+tushartyagi0507@users.noreply.github.com>
2026-04-05 15:56:03 +0530

    Update 20_valid_parentheses.js - LeetCode Problem 20

--- 20_valid_parentheses.js
+++ 20_valid_parentheses.js
@@ -2,25 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(s){
+var isValid = function(s) {
     let stack = [];
-
     let map = {
-        "{" : "}",
-        "[" : "]",
-        "(" : ")"
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    }
+    let size = s.length;
+    for (let i =0; i < size; i++){
+        if(map[s[i]]) stack.push(s[i]);
+        else{
+            if(stack.length == 0) return false; // nothing to match
+           let top = stack.pop();
+            if(s[i] != map[top]) return false
+        }
     }
 
-    for(let i=0; i < s.length; i++){
-        if(map[s[i]]) {
-            stack.push(s[i]);
-        }
-        else {
-            let top = stack.pop();
-            if(!top || s[i] != map[top]) {
-                return false;
-            }
-        }
-    }
     return stack.length === 0;
-}
+};
