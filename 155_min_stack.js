6d16a567b609e31efbf2fb23f1c1a195a2cec5b6
Tushar Tyagi <152903592+tushartyagi0507@users.noreply.github.com>
2026-04-07 00:27:20 +0530

    Update 155_min_stack.js - LeetCode Problem 155

--- 155_min_stack.js
+++ 155_min_stack.js
@@ -1,20 +1,50 @@
 var MinStack = function() {
-    this.s = [];
+    this.stack = [];
 };
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
 MinStack.prototype.push = function(val) {
-    if(this.s.length === 0){
-        this.s.push([val, val]);
-    } else {
-        let minVal = Math.min(val, this.s[this.s.length - 1][1]);
-        this.s.push([val, minVal]);
+    if (this.stack.length === 0)  this.stack.push([val, val]) 
+    else {
+        // find the min between the element which is being pushed and last min from top
+        // at every index we would have another array with value and MinValue till that 
+        // top element ke array ke 1 index par actual minimum saved hai 
+        currentMin = this.stack[this.stack.length - 1][1]
+        let min = Math.min(val, currentMin)
+        this.stack.push([val, min])
     }
+    
 };
+
+/**
+ * @return {void}
+ */
 MinStack.prototype.pop = function() {
-    this.s.pop();
+     this.stack.pop()
 };
+
+/**
+ * @return {number}
+ */
 MinStack.prototype.top = function() {
-    return this.s[this.s.length-1][0];
+    return this.stack[this.stack.length  - 1][0]
 };
+
+/**
+ * @return {number}
+ */
 MinStack.prototype.getMin = function() {
-    return this.s[this.s.length-1][1];
+    return this.stack[this.stack.length - 1][1];
 };
+
+/** 
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
