9618246c1e28c735b37f1090c570b44be618fbb6
Tushar Tyagi <152903592+tushartyagi0507@users.noreply.github.com>
2026-09-16 23:13:46 +0530

    Update 1_two_sum.py - LeetCode Problem 1

--- 1_two_sum.py
+++ 1_two_sum.py
@@ -1,13 +1,11 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         seen = {}
-        for i, value in enumerate(nums):
-            needed = target - value
-
+        for i in range(len(nums)):
+            needed = target - nums[i]
             if needed in seen:
-                return [i, seen[needed]]
-
-            seen[value] = i
+                return [seen[needed], i]
+            seen[nums[i]] = i
 
 solution = Solution()
 solution.twoSum([2,7,11,15], 9 )
